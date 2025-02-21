193cceb996175c0bf31b848a55fe1ed28ef7b2c2
natenadze1102 <natenadze1102@gmail.com>
2025-02-21 17:18:34 +0400

    add pretteir config

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,6 +4,6 @@ const API_PATHS = {
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://qeonfvidg3.execute-api.eu-central-1.amazonaws.com/prod',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-};
+}
 
-export default API_PATHS;
+export default API_PATHS
