128f302cf433ac8e7b4ee9f5a03a38e617c472fb
natenadze1102 <natenadze1102@gmail.com>
2025-03-08 23:17:32 +0400

    change api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://qeonfvidg3.execute-api.eu-central-1.amazonaws.com/prod',
+  import:
+    'https://vcio5jrwnf.execute-api.eu-central-1.amazonaws.com/prod/import',
+  bff: 'https://nkxzg11gig.execute-api.eu-central-1.amazonaws.com/prod',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-}
+};
 
-export default API_PATHS
+export default API_PATHS;
